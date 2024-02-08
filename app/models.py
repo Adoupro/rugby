5d94bc45d892f6861378d373be156fe2d4286215
Adou YAPO AKO <adou.professionnel@gmail.com>
2024-02-08 11:36:01 +0100

    Create my first model & html page

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): _description_
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return f"{self.last_name} - {self.first_name}"
